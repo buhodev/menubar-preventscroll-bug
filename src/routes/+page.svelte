14312e47f6ae2a0d16096c03735f9a812d050957
Carlos Ivanchuk <buhodev@proton.me>
2023-12-14 18:58:06 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="bg-blue-200 h-screen">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<div class="bg-emerald-200 h-screen"></div>
+<div class="bg-red-200 h-screen"></div>
+<div class="bg-orange-200 h-screen"></div>
+<div class="bg-indigo-200 h-screen"></div>
